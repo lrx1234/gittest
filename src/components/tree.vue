ac0c75bb8e2968f89972219439218190b7d85072
lirx <lirx@shindata.com>
2021-07-08 11:20:41 +0800

    log1

---
+++ src/components/tree.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{tree}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tree: "我是一棵树"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
